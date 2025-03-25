6579459684092e48cd92918cc795c5cd4fd8dc7b
future0923 <future94@qq.com>
2025-03-25 15:31:17 +0800

    structured-output

---
+++ java/structured-output/src/main/java/io/github/future0923/ai/agent/example/structured/output/StructuredOutputApplication.java
@@ -0,0 +1,15 @@
+package io.github.future0923.ai.agent.example.structured.output;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author future0923
+ */
+@SpringBootApplication
+public class StructuredOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredOutputApplication.class, args);
+    }
+}
